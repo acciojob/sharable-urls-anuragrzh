05f817b52f9ea2d28c1d085025fd2fce9b0b1ba6
anuragsoni5643 <sonianurag506@gmail.com>
2025-08-14 14:37:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 // your code here
  const form = document.getElementById('infoForm');
     const urlDisplay = document.getElementById('url');
+const baseURL = "https://localhost:8080/";
 
     form.addEventListener('submit', function(event) {
       event.preventDefault(); // Prevent actual form submission
+// console.log("form event")
+		const params = [];
 
       const name = document.getElementById('name').value;
       const year = document.getElementById('year').value;
 
-      const baseUrl = "https://localhost:8080/";
-      const queryString = `?name=${encodeURIComponent(name)}&year=${encodeURIComponent(year)}`;
+		if(name) params.push[`name=${encodedURIComponent(name)}`];
+		if(year) params.push[`year=${encodedURIComponent(year)}`];
 
-      urlDisplay.textContent = baseUrl + queryString;
+      urlDisplay.textContent = params.length ? `${baseUrl}?${params.join("&")}` : baseUrl;
     });
