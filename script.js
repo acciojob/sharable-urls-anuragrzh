3a731121d18e6514d7c534247464719f387c845d
anuragsoni5643 <sonianurag506@gmail.com>
2025-08-14 15:10:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-// // // your code here
-// //  const form = document.getElementById('infoForm');
-// //     const urlDisplay = document.getElementById('url');
-// // const baseURL = "https://localhost:8080/";
+// your code here
+ const form = document.getElementById('infoForm');
+    const urlDisplay = document.getElementById('url');
+const baseURL = "https://localhost:8080/";
 
-//     document.getElementById("button").addEventListener('click', function() {
-//       // event.preventDefault(); // Prevent actual form submission
-// // console.log("form event")
-// 		const params = [];
+    form.addEventListener('submit', function(event) {
+      event.preventDefault(); // Prevent actual form submission
 
-//       const name = document.getElementById('name').value;
-//       const year = document.getElementById('year').value;
+		const params = [];
 
-// 		if(name) params.push[`name=${encodeURIComponent(name)}`];
-// 		if(year) params.push[`year=${encodeURIComponent(year)}`];
+      const name = document.getElementById('name').value;
+      const year = document.getElementById('year').value;
 
-//       document.getElementById('url').textContent = params.length > 0 ? `https://localhost:8080/?${params.join("&")}` : "https://localhost:8080/";
-//     });
+		if(name) params.push(`name=${encodeURIComponent(name)}`);
+		if(year) params.push(`year=${encodeURIComponent(year)}`);
+
+      urlDisplay.textContent = params.length ? `${baseURL}?${params.join("&")}` : baseURL;
+    });
+
+// const h3Element = document.getElementById("url");
+// h3Element.textContent = urlDisplay;
