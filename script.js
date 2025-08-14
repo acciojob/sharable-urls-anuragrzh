3ebff3ff82e27b82408ad8343d4c0475c02c2e42
anuragsoni5643 <sonianurag506@gmail.com>
2025-08-14 14:45:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-// your code here
- const form = document.getElementById('infoForm');
-    const urlDisplay = document.getElementById('url');
-const baseURL = "https://localhost:8080/";
+// // your code here
+//  const form = document.getElementById('infoForm');
+//     const urlDisplay = document.getElementById('url');
+// const baseURL = "https://localhost:8080/";
 
-    form.addEventListener('submit', function(event) {
-      event.preventDefault(); // Prevent actual form submission
+    document.getElementById("button").addEventListener('click', function() {
+      // event.preventDefault(); // Prevent actual form submission
 // console.log("form event")
 		const params = [];
 
       const name = document.getElementById('name').value;
       const year = document.getElementById('year').value;
 
-		if(name) params.push[`name=${encodedURIComponent(name)}`];
-		if(year) params.push[`year=${encodedURIComponent(year)}`];
+		if(name) params.push[`name=${encodeURIComponent(name)}`];
+		if(year) params.push[`year=${encodeURIComponent(year)}`];
 
-      urlDisplay.textContent = params.length ? `${baseUrl}?${params.join("&")}` : baseUrl;
+      document.getElementById('url').textContent = params.length ? `https://localhost:8080/?${params.join("&")}` : "https://localhost:8080/";
     });
